c1c718cfaf8eb4a329586079cac4edba90590173
Guillermo Lopez <guillermolopez2529@gmail.com>
2018-09-16 23:35:01 -0400

    fix complete basic project structure

---
+++ README.md
@@ -0,0 +1,37 @@
+# order
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run dev
+```
+
+### Run in debug mode
+```
+npm run debug
+```
+
+### Run in debug brk mode
+```
+npm run debug-brk
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints files
+```
+npm run lint
+```
+
+### Run in production mode
+```
+npm run start
+```
+
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 'use strict'
 
 const http = require('http')
+const helmet = require('helmet')
 const express = require('express')
 const { Nuxt, Builder } = require('nuxt')
-const { getLogger } = require('@restood/utils')
+const { getLogger, terminate } = require('@restood/utils')
 const config = require('./nuxt.config')
 
 const logger = getLogger('apps/order')
 const app = express()
 const server = http.createServer(app)
-const port = process.env.PORT || 3000
+const port = +process.env.PORT || 3000
 
 const nuxt = new Nuxt(config)
 
-if (config.dev) {
+if (!module.parent && config.dev) {
   const builder = new Builder(nuxt)
   builder.build()
 }
 
+app.use(helmet())
 app.use(nuxt.render)
 
-server.listen(port, () => {
-  logger.info(`Server listening on http://localhost:${port}`)
-})
+if (!module.parent) {
+  server.listen(port, () => {
+    logger.info(`Server listening on http://localhost:${port}`)
+
+    process.on('SIGINT', terminate(0, 'SIGINT'))
+    process.on('SIGTERM', terminate(0, 'SIGTERM'))
+    process.on('uncaughtException', terminate(1, 'uncaughtException'))
+    process.on('unhandledRejection', terminate(1, 'unhandledRejection'))
+  })
+}
